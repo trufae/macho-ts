2a32d2f84ec9abf5e6982f5f07d4ce7a32bc2462
Håvard Sørbø <havard@hsorbo.no>
2023-08-31 15:34:05 +0200

    Convert to esm module

--- test/api-test.ts
+++ test/api-test.ts
@@ -1,4 +1,4 @@
-import macho from "../dist/macho.js";
+import macho from "../lib/macho.js";
 import fs from "fs";
 import assert from "assert";
 
@@ -7,10 +7,10 @@ describe('macho', function() {
     const exe = macho.parse(fs.readFileSync("./test/bins/ls"));
     assert(exe.bits === 64 || exe.bits === 32);
     assert.equal(exe.filetype, 'execute');
-    assert(exe.cmds.some(function(cmd) {
+    assert(exe.cmds.some(function(cmd:any) {
       return cmd.type === "dysymtab" && cmd.nindirectsyms === 174;
     }));
-    assert(exe.cmds.some(function(cmd) {
+    assert(exe.cmds.some(function(cmd:any) {
       return cmd.type === "function_starts" &&
              cmd.addresses.length;
     }));
